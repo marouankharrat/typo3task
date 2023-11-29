be34c34c6b31f2ee934531166b17baf02eb3d8b2
m.kharrat <m.kharrat@signup-design.de>
2023-11-29 12:01:49 +0100

    install typo3 11 with composer and change in gitignone file

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # typo3task
-Typo3 12 + Extenstion "Link of the month" display on a page
+Typo3 11 + Extenstion "Link of the month" display on a page
+
+# Link of the month
+
+New TYPO3 11 via composer
+
+
+##Install
+Run in Terminal <br />
+`composer install`
+
+##Folder structure in TYPO3 composer mode
+Folders:<br />
+* `/config`:<br />
+siteconfig goes here (`/config/sites/_siteconfigname_/config.yaml`)
+* `/public`: <br />
+Holds `/typo3conf/` with extensions, `/fileadmin/`, `/typo3/` and `/typo3temp/`
+* `/var/`:<br />
+Holds TYPO3 Logs etc
+* `/vendor/`:<br />
+Holds all TYPO3 vendor dependencies
+
+Access to Admin Panel:
+Login: codered
+Pwd: willkommen
---
+++ taskproject/public/typo3conf/LocalConfiguration.php
@@ -0,0 +1,122 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$eWFOZTVGU2J5SEtNcFByTQ$ZdEFsNI+uwfhuphJ2zDSCf86va0h4Y4y8jC8s5XKFa8',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'typo3task',
+                'driver' => 'mysqli',
+                'host' => '127.0.0.1',
+                'password' => 'typo3task',
+                'port' => 8889,
+                'tableoptions' => [
+                    'charset' => 'utf8mb4',
+                    'collate' => 'utf8mb4_unicode_ci',
+                ],
+                'user' => 'typo3task',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+    ],
+    'FE' => [
+        'cacheHash' => [
+            'enforceValidation' => true,
+        ],
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/sbin/sendmail -t -i',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pagesection' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '9fc13543edd56a36b3ac3aa00e1377acb27b16a5c8f7713f84e439ba485f7baccac82f8ab86cd9653f28496acb2dd8e1',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'yamlImportsFollowDeclarationOrder' => true,
+        ],
+        'sitename' => 'Typo3 Task',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
